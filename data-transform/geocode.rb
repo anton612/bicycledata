856a72dc5bb10513c775634b4d6cdff7f8d4726d
Kristina <kdurivage@gelicia.com>
2015-06-24 17:31:36 -0500

    Give up node, do it in ruby like a normal person

---
+++ data-transform/geocode.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'geocoder'
+require 'csv'
+
+outfile = File.open('temp.csv', 'w')
+outfile.write("Crash ID,County,Community,Primary Road,Intersecting Road,Date,Time,Type,Level,Crash Rpt.,long,lat\n")
+
+CSV.foreach("../Bicyclist-Motorist_Crashes_2000-2013.csv", {:headers=>:first_row}) do |row|
+	intersectionString = row["Primary Road"] + " and " + row["Intersecting Road"] + " " + row["Community"];
+
+	coords = Geocoder.coordinates(intersectionString)
+
+	outfile.write(row.to_csv(row_sep: nil) + "," + coords[0].to_s + "," +  coords[1].to_s + "\n");
+	sleep(0.2) #slow it down to get around the rate limit
+end
+outfile.close
